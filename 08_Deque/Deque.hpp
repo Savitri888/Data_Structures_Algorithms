8b018a34690d907dc790744dac760d31c72bfd02
Savitri Harkhu <sharkhu3649@floridapoly.edu>
2026-09-14 19:09:42 +0000

    Deque

---
+++ 08_Deque/Deque.hpp
@@ -0,0 +1,36 @@
+#ifndef DEQUE_HPP
+#define DEQUE_HPP
+
+template <typename T, int size = 100>
+class Deque {
+public:
+    Deque();
+
+    bool        empty() const;
+    bool        full() const;
+    void        print() const;
+    void        push_back(const T& val);
+    void        push_front(const T& val);
+
+
+    T    pop_front();
+    T    pop_back();
+
+    
+
+    // TODO
+    const T&    front(); // Return the first element in the queue
+    const T&    back(); // Return the last element in the queue
+    void        clear(); // Remove all the elements from the queue
+    int         size();  // Return the size of the queue
+
+    
+private:
+    T data[size];
+    int front_idx, back_idx;
+    int queue_size;
+};
+
+#include "Deque.tpp"
+
+#endif
